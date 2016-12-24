42a831226c5a628245889a4f9546c31af43bb7e1
Hammad Naseem <hammadnsm@gmail.com>
2016-12-24 16:51:38 -0600

    Implement CORS

--- js/stocks.js
+++ js/stocks.js
@@ -9,6 +9,8 @@ $(document).ready(function () {
         console.log(name);
         $.ajax({
             type: 'GET'
+            , dataType: 'JSON'
+            , crossDomain: true
             , url: "https://finance.google.com/finance/info?client=ig&q=NSE:" + name
             , success: function (response) {
                 response = response.substring(6, response.length);

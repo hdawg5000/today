d1436f2597d1299a02f4f72476b547c826b98533
Hammad Naseem <hammadnsm@gmail.com>
2016-12-24 20:56:41 -0600

    Added input for adding stock symbols to the list

--- js/stocks.js
+++ js/stocks.js
@@ -33,14 +33,10 @@ function loadStocks(response, name) {
     if (response.t == null) {
         $('#stocks').append('<tr class="failed">Failed to retrieve stock data for ' + name + '</tr>');
     } else {
-
-        //Add stock price to table. If price > 0, set id to positive. Else, set to negative
-        var price = response.c;
-        price = price.substring(0, 1) + " $" + price.substring(1, price.length);
         if (response.c > 0) {
-            $('#stocks').append('<tr><td>' + response.t + '</td><td id="positive">$' + response.l + '</td><td id="positive">' + price + '</td></tr>');
+            $('#stocks').append('<tr><td>' + response.t + '</td><td id="positive">$' + response.l + '</td><td id="positive">' + response.c + '</td></tr>');
         } else if (response.c < 0) {
-            $('#stocks').append('<tr><td>' + response.t + '</td><td id="negative">$' + response.l + '</td><td id="negative">' + price + '</td></tr>');
+            $('#stocks').append('<tr><td>' + response.t + '</td><td id="negative">$' + response.l + '</td><td id="negative">' + response.c + '</td></tr>');
         }
     }
 }
@@ -55,24 +51,50 @@ function refreshStockPrices() {
         console.log(name);
         $.ajax({
             type: 'GET'
-            , url: "http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=" + name
-            , success: function (response, name) {
-                loadStocks(response, name);
+            , url: "https://finance.google.com/finance/info?client=ig&q=NSE:" + name
+            , success: function (response) {
+                response = response.substring(6, response.length);
+                response = response.substring(0, response.length - 3);
+                response = JSON.parse(response);
+                loadStocks(response);
             }
-            , error: function (xhr, textStatus, errorMessage) {
-                console.log(xhr.status);
-                if (xhr.status == 501) {
-                    $.ajax(this);
-                    return;
-                }
-
-                //$('#stocks').append('<tr class="failed">Failed to retrieve stock data for ' + name + '</tr>');
+            , error: function () {
+                $('#stocks').append('<tr class="failed">Failed to retrieve stock data for ' + name + '</tr>');
             }
         });
     }
 }
 
 // Add stock names to stocks array
-function addStockName(name) {
-    stocks[stocks.length] = name;
+function addStockName() {
+    var exists = false;
+    var sym = document.getElementById("symbol").value;
+    if (sym !== null) {
+        sym = sym.toUpperCase();
+        console.log(sym);
+        $('#symbol').attr('disabled', 'disabled');
+        $('#sub_sym').attr('disabled', 'disabled');
+
+        for (var i = 0; i < stocks.length; i++) {
+            if (stocks[i] == sym) {
+                exists = true;
+                break;
+            }
+        }
+
+        if (!exists) {
+            //check if symbol is valid symbol
+            var valid = false;
+
+            stocks[stocks.length] = sym;
+            refreshStockPrices();
+            document.getElementById("symbol").value = '';
+        } else {
+            //highlight existing table row
+            document.getElementById("symbol").value = '';
+            $('.error_msg').show().delay(5000).fadeOut(1000);
+        }
+        $('#symbol').removeAttr('disabled');
+        $('#sub_sym').removeAttr('disabled');
+    }
 };
